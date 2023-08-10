434f0afc0f9d77f4a0b17bb2e3a464031ef6f3c0
Meet Patel <meetaiims@yahoo.com>
2023-08-10 06:42:48 -0400

    implement memoization to optimize fibonacci recursive computations

--- fibonacci_seq.py
+++ fibonacci_seq.py
@@ -1,15 +1,36 @@
 def find_fibonacci_iter(n):
     f0 = 0
     f1 = 1
-    for i in range(n+1):
-        if i == 0:
-            print(f0)
-        if i == 1:
-            print(f1)
-        if i > 1:
-            current_fib = f0 + f1
-            print(current_fib)
-            f0 = f1
-            f1 = current_fib
+    if type(n) != int:
+        raise TypeError(f"Input argument should be of type 'int' while it was of type '{type(n)}'")
+    if n < 0:
+        raise Exception("Input argument should be greater than or equal to 0")
+    if n == 0:
+        return f0
+    if n == 1:
+        return f1
+    for i in range(2, n+1):
+        current_fib = f0 + f1
+        f0 = f1
+        f1 = current_fib
+    return current_fib
 
-find_fibonacci_iter(8)
+# making recursive fibonacci function more efficient using memoization
+fibonacci_cache = {0: 0, 1: 1}
+def find_fibonacci_recur(n):
+    if type(n) != int:
+        raise TypeError(f"Input argument should be of type integer but instead was of type {type(n)}")
+    if n < 0:
+        raise ValueError("Input argument should be a natural number")
+    if n in fibonacci_cache:
+        return fibonacci_cache[n]
+    else:
+        value = find_fibonacci_recur(n-2) + find_fibonacci_recur(n-1)
+    fibonacci_cache[n] = value
+    return value
+
+# 100: 354224848179261915075
+for i in range(11):
+    print(f"{i}: {find_fibonacci_recur(i)}")
+
+print(find_fibonacci_recur(3.3))
