809d434b62d57d55490803ac94abae309eb4b007
Meet Patel <meetaiims@yahoo.com>
2023-08-20 16:07:02 -0400

    find missing num using formula

--- find_missing_num.py
+++ find_missing_num.py
@@ -1,15 +1,14 @@
-def find_missing_num(n):
-    curr_num = n[0]
-    diff = 1
-    missing_num = []
-    for i, j in enumerate(n):
-        if i != 0:
-            diff = n[i] - curr_num
-            if diff != 1:
-                for k in range(diff-1):
-                    missing_num.append(curr_num+(k+1))
-        curr_num = n[i]
-    return missing_num
+def find_missing_num(arr):
+    n = len(arr)
+    for i in range(n):
+        if i not in arr:
+            return i
 
-arr = find_missing_num([0,1,2,4,7, 12])
-print(arr)
+def find_missing_num_2(arr):
+    n = len(arr) + 1
+    expected_sum = int(n*(n-1)/2)
+    return expected_sum - sum(arr)
+
+arr = [0,1,2,4,5] 
+print(find_missing_num(arr))
+print(find_missing_num_2(arr))
